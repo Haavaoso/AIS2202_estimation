3a7be8e59b6f34d674b822ea91086a89f40dcd7f
Håvard Olai Solbakken <haavaoso@stud.ntnu.no>
2024-09-30 10:42:33 +0200

    Test simon plz

--- parameter_estimation/main.cpp
+++ parameter_estimation/main.cpp
@@ -1,4 +1,6 @@
-#include <rapidcsv.h>
+#include <iostream>
+#include <vector>
+#include "rapidcsv.h"
 #include <Eigen/Dense>
 
 Eigen::MatrixXd pseudo_inverse(const Eigen::MatrixXd &A)
@@ -8,10 +10,20 @@ Eigen::MatrixXd pseudo_inverse(const Eigen::MatrixXd &A)
 
 int main()
 {
+
+    rapidcsv::Document doc("0-calibration_fts-accel.csv");
+
     Eigen::VectorXd G(72);
     Eigen::VectorXd F(72);
 
-    Eigen::Vector3d r = (G.transpose()*G)
 
+
+    /*
+    //the pseudo_inverse function is applied to the term G.transpose() * G because you cannot
+    //directly divide matrices. The pseudo-inverse takes the place of division.
+    Eigen::Vector3d r = pseudo_inverse(G.transpose() * G) * (G.transpose() * F);
+    */
+
+    std::cout << "r" << std::endl;
     return 0;
 }
