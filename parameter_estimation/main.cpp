9dadaf6431955d08107eb7be56eed5757e265850
Håvard Olai Solbakken <haavaoso@stud.ntnu.no>
2024-09-30 12:13:20 +0200

    goat f Bias funke

--- parameter_estimation/main.cpp
+++ parameter_estimation/main.cpp
@@ -7,23 +7,51 @@ Eigen::MatrixXd pseudo_inverse(const Eigen::MatrixXd &A)
 {
     return A.completeOrthogonalDecomposition().pseudoInverse();
 }
-
+float fx_bias;
 int main()
 {
-
-    rapidcsv::Document doc("0-calibration_fts-accel.csv");
-
-    Eigen::VectorXd G(72);
-    Eigen::VectorXd F(72);
-
-
-
     /*
-    //the pseudo_inverse function is applied to the term G.transpose() * G because you cannot
-    //directly divide matrices. The pseudo-inverse takes the place of division.
-    Eigen::Vector3d r = pseudo_inverse(G.transpose() * G) * (G.transpose() * F);
+    // Check if the file exists by opening it
+    std::ifstream file();
+    if (!file)
+    {
+        std::cerr << "Error: OOOF!" << std::endl;
+        return -1;
+    }
     */
 
-    std::cout << "r" << std::endl;
+    // Use least squares to estimate the (constant) mass and mass center of an unknown robot end effector tool.
+
+    try {
+        rapidcsv::Document doc("C:/libs/dataset/0-calibration_fts-accel.csv");
+
+
+
+        try {
+            std::vector<float> fx = doc.GetColumn<float>("fx");
+            std::cout << "JIPPU:: " << fx.size() << std::endl;
+            float buffer = 0;
+            for (int i = 0; i < fx.size(); i++) {
+                buffer += fx[i];
+            }
+            fx_bias = buffer/fx.size();
+
+        } catch (const std::invalid_argument& e) {
+            std::cerr << "Error: Fuck dette" << e.what() << std::endl;
+        }
+
+
+        std::cout << "Doc loaded: ";
+
+
+
+    } catch (...) {
+        std::cerr << "Error: Did not load doc" << std::endl;
+    }
+
+
+
+
+    std::cout << fx_bias << std::endl;
     return 0;
 }
