1b73c10aedba66c706b8fb9cbf75b09f52290062
Håvard Olai Solbakken <haavaoso@stud.ntnu.no>
2024-09-30 14:42:02 +0200

    Now taking in matrices and saving them

--- parameter_estimation/main.cpp
+++ parameter_estimation/main.cpp
@@ -7,40 +7,30 @@ Eigen::MatrixXd pseudo_inverse(const Eigen::MatrixXd &A)
 {
     return A.completeOrthogonalDecomposition().pseudoInverse();
 }
-float fx_bias;
+
+
+bool file_exists(const std::string& name) {
+    std::ifstream f(name.c_str());
+    return f.good();
+}
 int main()
 {
-    /*
-    // Check if the file exists by opening it
-    std::ifstream file();
-    if (!file)
-    {
-        std::cerr << "Error: OOOF!" << std::endl;
+    std::string filePath = "../dataset/0-calibration_fts-accel.csv";
+    if (!file_exists(filePath)) {
+        std::cerr << "Error: File does not exist at " << filePath << std::endl;
         return -1;
     }
-    */
-
     // Use least squares to estimate the (constant) mass and mass center of an unknown robot end effector tool.
 
     try {
-        rapidcsv::Document doc("C:/libs/dataset/0-calibration_fts-accel.csv", rapidcsv::LabelParams(-1, -1));
-
-
+        rapidcsv::Document doc(filePath);
 
         std::vector<std::vector<float>> bufferVector(6);
         std::vector<float> biasVector(6);
 
-
         for (int i = 0; i < biasVector.size(); i++) {
             bufferVector[i] = doc.GetColumn<float>(i);
         }
-
-        /*std::cout << "loaded colums" << std::endl;
-        std::cout << bufferVector[0].size() << std::endl;
-        for (int i = 0; i < bufferVector[0].size(); i++) {
-            std::cout << i << ": " << bufferVector[0][i] << std::endl;
-        }*/
-
 
         for (int i = 0; i < bufferVector.size(); i++) {
             float buffer = 0;
@@ -48,22 +38,65 @@ int main()
                 //std::cout << bufferVector[i][j] << std::endl;
                 buffer += bufferVector[i][j];
             }
-
-            //std::cout << "NEW" << std::endl;
             biasVector[i] = buffer/bufferVector[i].size();
-            //std::cout << biasVector[i] << std::endl;
         }
 
 
         for (int i = 0; i < biasVector.size(); i++) {
             std::cout << i << ": " << biasVector[i] << std::endl;
-            //std::cout << bufferVector[i].size() << std::endl;
         }
 
-        std::cout << "Doc loaded: ";
+        //Create IMU and FTS vector and fill it
+        std::vector<std::vector<float>> imuVector(3);
+        std::vector<std::vector<float>> ftsVector(3);
 
 
+        for (int i = 6; i < imuVector.size(); i++) {
+            imuVector[i] = doc.GetColumn<float>(i);
+        }
+        for (int i = 9; i < ftsVector.size(); i++) {
+            ftsVector[i] = doc.GetColumn<float>(i);
+        }
 
+        //Create rotation matrix of the FTS
+        std::vector<std::vector<float>> r1x(3);
+        std::vector<std::vector<float>> r2x(3);
+        std::vector<std::vector<float>> r3x(3);
+
+        std::vector<Eigen::Matrix3f> rotationMatrices(24);
+
+        for (int i = 0; i < 3; i++) {
+            r1x[i] = doc.GetColumn<float>(i+12);
+            r2x[i] = doc.GetColumn<float>(i+15);
+            r3x[i] = doc.GetColumn<float>(i+18);
+        }
+
+
+        for (int i = 0; i < rotationMatrices.size(); i++) {
+            for (int j = 0; j < 3; j++) {
+                rotationMatrices[i](0, j) = r1x[j][i];
+                rotationMatrices[i](1, j) = r2x[j][i];
+                rotationMatrices[i](2, j) = r3x[j][i];
+            }
+        }
+
+        //Test Matrix
+        /*
+        Eigen::MatrixXd ftsMat(3, 3);  // Creates a 3x3 matrix of doubles
+        ftsMat(0, 0) = -1;
+        ftsMat(1, 0) = 5.51269E-06;
+        ftsMat(2, 0) = 0.000170476;
+        ftsMat(0, 1) = 0.000170476;
+        ftsMat(1, 1) = 1.20441E-05;
+        ftsMat(2, 1) = 1;
+        ftsMat(0, 2) = 5.51063E-06;
+        ftsMat(1, 2) = 1;
+        ftsMat(2, 2) = -1.20451E-05;
+        std::cout << ftsMat << std::endl;
+        */
+
+
+        std::cout << rotationMatrices[0] << std::endl;
     } catch (...) {
         std::cerr << "Error: Did not load doc" << std::endl;
     }
