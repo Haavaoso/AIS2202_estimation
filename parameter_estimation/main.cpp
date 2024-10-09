e00e205939f7229eedb52daff46121e086b7e4dc
Håvard Olai Solbakken <haavaoso@stud.ntnu.no>
2024-10-09 15:07:40 +0200

    klar for state_space_model

--- parameter_estimation/main.cpp
+++ parameter_estimation/main.cpp
@@ -3,6 +3,9 @@
 #include "rapidcsv.h"
 #include <Eigen/Dense>
 #include "include/ParameterEstimation.h"
+#include "include/variance.h"
+
+
 
 int main() {
     rapidcsv::Document doc("../dataset/0-calibration_fts-accel.csv");
@@ -14,6 +17,27 @@ int main() {
     std::cout << "CLASS: " << param.getMassEstimate() << std::endl;
 
     std::cout << "CLASS: " << param.getCenterMassVector() << std::endl;
+
+
+
+
+    rapidcsv::Document doc0("../dataset/0-steady-state_accel.csv");
+    rapidcsv::Document doc1("../dataset/0-steady-state_wrench.csv");
+
+    double varAx = variance(doc0, 0);
+    double varAy = variance(doc0, 1);
+    double varAz = variance(doc0, 2);
+
+    double varFx = variance(doc0, 2);
+    double varFy = variance(doc0, 1);
+    double varFz = variance(doc0, 2);
+    double varTx = variance(doc0, 3);
+    double varTy = variance(doc0, 4);
+    double varTz = variance(doc0, 5);
+
+
+
+
 
     return 0;
 }
